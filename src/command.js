bbd0dd29e1a5256b11695d97c68ca4e43ca8355a
Masaru Hori <qirex_rr@yahoo.co.jp>
2021-12-31 14:11:00 +0900

    delete block.js

---
+++ src/command.js
@@ -0,0 +1,151 @@
+import Panel from './panel';
+
+const command_data = [
+    {   // 0
+        no: 0,
+        moji: '1',
+        func: set_value,
+        selected_style: { background: 'pink', border: 'solid 5px black'},
+    },
+    { 
+        no: 1,
+        moji: '2',
+        func: set_value,
+        selected_style: { background: 'pink', border: 'solid 5px black'},
+    },
+    { 
+        no: 2,
+        moji: '3',
+        func: set_value,
+        selected_style: { background: 'pink', border: 'solid 5px black'},
+    },
+    { 
+        no: 3,
+        moji: '4',
+        func: set_value,
+        selected_style: { background: 'pink', border: 'solid 5px black'},
+    },
+    { 
+        no: 4,
+        moji: '5',
+        func: set_value,
+        selected_style: { background: 'pink', border: 'solid 5px black'},
+    },
+  {
+    no: 5,
+    moji: '',
+    func: set_color,
+    selected_style: {background: 'seagreen', border: 'solid 5px black', },
+    base_style: {background: 'seagreen', border: 'solid 1px grey', },
+  },
+  {
+    no: 6,
+    moji: '',
+    func: set_color,
+    selected_style: { background: 'royalblue', border: 'solid 5px black', },
+    base_style: {background: 'royalblue', border: 'solid 1px grey', },
+  },
+  {
+    no: 7,
+    moji: '',
+    func: set_color,
+    selected_style: { background: 'gold', border: 'solid 5px black', },
+    base_style: {background: 'gold', border: 'solid 1px grey', },
+  },
+  {
+    no: 8,
+    moji: '',
+    func: set_color,
+    selected_style: { background: 'hotpink', border: 'solid 5px black', },
+    base_style: {background: 'hotpink', border: 'solid 1px grey', },
+  },
+  {
+    no: 9,
+    moji: '',
+    func: delete_color,
+    selected_style: { background: 'moccasin', border: 'solid 5px black', },
+    base_style: {background: 'moccasin', border: 'solid 1px grey', },
+  },
+  {
+    no: 14,
+    moji: 'DEL',
+    func: delete_value,
+    selected_style: { background: 'moccasin', border: 'solid 5px black'},
+    base_style: { background: 'moccasin', border: 'solid 1px grey'},
+  },
+  {
+    no: 11,
+    moji: 'UNDO',
+    base_style: { background: 'midnightblue', color: 'white', border: 'solid 1px grey'},
+  },
+  {
+    no: 12,
+    moji: 'REDO',
+    base_style: { background: 'darkslategrey', color: 'white', border: 'solid 1px grey'},
+  },
+  {
+    no: 10,
+    moji: 'CLS',
+    base_style: { background: 'dimgrey', color: 'white', border: 'solid 1px grey'},
+  },
+];
+
+function set_value(block, command) {
+  const new_block = JSON.parse(JSON.stringify(block));
+  new_block.value = command.no + 1;
+
+  return new_block;
+}
+
+function delete_value(block, command) {
+  const {
+    value,
+    color,
+    ...new_block 
+  } = JSON.parse(JSON.stringify(block));
+
+  return new_block;
+}
+
+function set_color(block, command) {
+  const new_block = JSON.parse(JSON.stringify(block));
+  new_block.color =command.base_style['background'];
+
+  return new_block;
+}
+
+function delete_color(block, command) {
+  const {
+    color,
+    ...new_block 
+  } = JSON.parse(JSON.stringify(block));
+
+  return new_block;
+}
+
+//----------------------------------------
+
+function Command({command, onSelect}) {
+    const selectedStyle = (i_command) => {
+      if (!i_command) return {};
+  
+      const base_style = i_command.base_style ? i_command.base_style : {background: 'transparent', border: 'solid 1px black'};
+      
+      if (!command) { 
+        return base_style;
+      }
+      const selected_style = command.selected_style || base_style;
+      
+      return command.no === i_command.no ? selected_style : base_style;
+    }
+    
+    return (
+      <div style={{position:'relative', height:160}}>
+        <Panel panel={command_data} onSelect={(no) => onSelect(command_data.find(e => e.no === no))} 
+               selectedStyle={ i => selectedStyle(command_data.find(e => e.no === i)) }
+        />
+      </div>
+    );
+}
+
+export default Command;
---
+++ src/panel.js
@@ -0,0 +1,19 @@
+function Panel({panel, onSelect, yoko=5, size=50, style={}, selectedStyle = (i) => { return {background: 'transparent'}}}) {
+  return (
+    <>
+      {panel.map((v, i) => {
+        const no = Number.isInteger(v.no) ? v.no : i;
+	const moji = v.moji || '';
+        return (<button key={no} 
+                        style={{width:size+'px', height:size+'px', position: 'absolute', top:Math.floor(no / yoko) * size, left: (no % yoko) * size,
+                                ...selectedStyle(no),
+                                ...style}}
+                        onClick={ () => onSelect(no) }>
+                  {moji}
+                </button>)
+      })}
+    </>
+  );
+}
+
+export default Panel;
